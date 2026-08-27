17e1c1c52cf23ed3b23cfbc0d53ba9a0963a59ae
verdonstudio <verdonstudio@gmail.com>
2026-08-27 09:51:53 +0200

    Update config.js

--- app_src/config.js
+++ app_src/config.js
@@ -1,1 +1,7 @@
-
+{
+  "reservations": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=1217681389#gid=1217681389",
+  "planning": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=589949075#gid=589949075",
+  "bilan": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=930480678#gid=930480678",
+  "yield": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=1211299443#gid=1211299443",
+  "airbnb": "https://www.airbnb.fr/hosting/messages"
+}
