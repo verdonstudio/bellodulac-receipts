7125b538e6f3383007c34d7751dd43f9a900dff4
verdonstudio <verdonstudio@gmail.com>
2026-08-27 17:58:59 +0200

    Update config.js

--- app_src/config.js
+++ app_src/config.js
@@ -1,7 +1,7 @@
-{
-  "reservations": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=1217681389#gid=1217681389",
-  "planning": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=589949075#gid=589949075",
-  "bilan": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=930480678#gid=930480678",
-  "yield": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=1211299443#gid=1211299443",
-  "airbnb planning": "https://www.airbnb.fr/multicalendar"
-}
+[
+  { "label": "📅 Réservations", "url": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=1217681389#gid=1217681389" },
+  { "label": "🗓️ Planning", "url": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=589949075#gid=589949075" },
+  { "label": "📊 Bilan Annuel", "url": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=930480678#gid=930480678" },
+  { "label": "📈 Yield Manager", "url": "https://docs.google.com/spreadsheets/d/1LX6Pkxu4FVwbNXE5g4P0wevjWwfXYMpKck8tz1J7NuU/edit?gid=1211299443#gid=1211299443" },
+  { "label": "🏠 AirBnb", "url": "https://www.airbnb.fr/multicalendar" }
+]
